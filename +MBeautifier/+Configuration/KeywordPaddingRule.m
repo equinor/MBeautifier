3d39fcc0df5c0acdec47f895c2f03b2d520f1625
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2020-07-07 20:28:34 +0200

    feat: added equality comparators eq(obj,objB)
    feat: added input to function MBeautify.getConfiguration to allow loading of custom xml-file and changed access to public.
    
    feat: default input value of MBeautify.formatFiles fileFilter as '*.m'
    
    feat: formatFiles can now recurse subfolders
    
    feat: Configuration rules updaters supports rules as inputs as well as values
    
    chore: added testfunction for config editor and comparators

--- +MBeautifier/+Configuration/KeywordPaddingRule.m
+++ +MBeautifier/+Configuration/KeywordPaddingRule.m
@@ -1,28 +1,57 @@
 classdef KeywordPaddingRule
-
+    
     properties (SetAccess = immutable)
         Keyword
         RightPadding
     end
-
+    
     properties (Dependent)
         ReplaceTo
     end
-
+    
     methods
         function obj = KeywordPaddingRule(keyword, rightPadding)
             obj.Keyword = lower(keyword);
             obj.RightPadding = rightPadding;
         end
-
+        
         function value = get.ReplaceTo(obj)
             wsPadding = '';
             for i = 1:obj.RightPadding
                 wsPadding = [' ', wsPadding];
             end
-
+            
             value = [obj.Keyword, wsPadding];
         end
+        
+        function TF = eq(obj,objB)
+            % Returns true if two KeyWordPaddingRule objects have the same values.
+            % TF = eq(obj,objB)
+            TF = false;
+            if ~isa(objB,class(obj))
+                return
+            end
+            prop = properties(obj);
+            for k = 1:numel(prop)
+                if ~isprop(objB,prop{k})
+                    return
+                end
+                
+                if ~isa(objB.(prop{k}),class(obj.(prop{k})))
+                    return
+                end
+                
+                if ischar(objB.(prop{k}))
+                    if ~strcmp(obj.(prop{k}),objB.(prop{k}))
+                        return
+                    end
+                else
+                    if ~(obj.(prop{k})  == objB.(prop{k}))
+                        return
+                    end
+                end
+            end
+            TF = true;
+        end
     end
-
 end
--- +MBeautifier/+Configuration/SpecialRule.m
+++ +MBeautifier/+Configuration/SpecialRule.m
@@ -1,23 +1,56 @@
 classdef SpecialRule
-
+    
     properties (SetAccess = immutable)
         Key
         Value
     end
-
+    
     properties (Dependent)
         ValueAsDouble
     end
-
+    
     methods
         function obj = SpecialRule(key, value)
             obj.Key = key;
             obj.Value = value;
         end
-
+        
         function value = get.ValueAsDouble(obj)
             value = str2double(obj.Value);
         end
+        
+        function TF = eq(obj,objB)
+            % Returns true if two SpecialRule objects have the same values.
+            % TF = eq(obj,objB)
+            TF = false;
+            if ~isa(objB,class(obj))
+                return
+            end
+            prop = properties(obj);
+            for k = 1:numel(prop)
+                if ~isprop(objB,prop{k})
+                    return
+                end
+                
+                if ~isa(objB.(prop{k}),class(obj.(prop{k})))
+                    return
+                end
+                
+                if ischar(objB.(prop{k}))
+                    if ~strcmp(obj.(prop{k}),objB.(prop{k}))
+                        return
+                    end
+                elseif isnumeric(obj.(prop{k}))
+                    if ~isequaln(obj.(prop{k}),objB.(prop{k}))
+                        return
+                    end
+                else
+                    if ~(obj.(prop{k}) == objB.(prop{k}))
+                        return
+                    end
+                end
+            end
+            TF = true;
+        end
     end
-
 end
